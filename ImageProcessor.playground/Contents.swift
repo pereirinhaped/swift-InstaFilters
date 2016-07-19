8b93d6a3b526f029693d692d188761584ef67398
Pedro Pereirinha <pereirinhaped@gmail.com>
2016-07-20 00:21:32 +0100

    First Commit - ToGrayScale implemented

--- ImageProcessor.playground/Contents.swift
+++ ImageProcessor.playground/Contents.swift
@@ -4,24 +4,23 @@ import UIKit
 
 let image = UIImage(named: "sample")
 
-let sharedFilterUtils = FilterUtils()
-
 // Process the image!
 
 
 // Instantiate the RGBAImage class
 
 let rgbaImg = RGBAImage.init(image: image!)
-var toEditImg = ToGrayScale().turnToGrayScale(rgbaImg!, withAlpha: false)
-var toEditImg2 = ToGrayScale().turnToGrayScale(rgbaImg!, withAlpha: true)
 
-
-
-
+// Apply ToGrayScale Filter
+var toEditImg = ToGrayScale().turnToGrayScale(rgbaImg!)
+var toEditImg2 = ToGrayScale().turnToGrayScale(rgbaImg!, intensity: ToGrayScale.Intensity.Strong)
+var toEditImg3 = ToGrayScale().turnToGrayScale(rgbaImg!, intensity: ToGrayScale.Intensity.Medium)
+var toEditImg4 = ToGrayScale().turnToGrayScale(rgbaImg!, intensity: ToGrayScale.Intensity.Low)
 
 
 // Create new UIImage for display
 
 let newImage = toEditImg.toUIImage()
-
 let newImage2 = toEditImg2.toUIImage()
+let newImage3 = toEditImg3.toUIImage()
+let newImage4 = toEditImg4.toUIImage()
--- ImageProcessor.playground/Sources/ToGrayScale.swift
+++ ImageProcessor.playground/Sources/ToGrayScale.swift
@@ -6,19 +6,49 @@ public class ToGrayScale: FilterUtils {
 		super.init()
 	}
 	
-	public func turnToGrayScale(anImg: RGBAImage, withAlpha: Bool) -> RGBAImage {
+	public enum Intensity: Float {
+		case Low
+		case Medium
+		case Strong
+		}
+	
+	public func turnToGrayScale(anImg: RGBAImage) -> RGBAImage {
 		
 		var newImage = anImg
 		var pixel = newImage.pixels
 		
 		for index in 0..<newImage.pixels.count {
-			let pixelAvg = getPixelAvgVal(pixel[index], AlphaOn: withAlpha)
+			let pixelAvg = getPixelAvgVal(pixel[index], AlphaOn: false)
 			pixel[index].red = pixelAvg
 			pixel[index].green = pixelAvg
 			pixel[index].blue = pixelAvg
-			if withAlpha {
-				pixel[index].alpha = pixelAvg
-			}
+			newImage.pixels[index] = pixel[index]
+		}
+		return newImage
+	}
+	
+	public func turnToGrayScale(anImg: RGBAImage, intensity: Intensity) -> RGBAImage {
+		
+		var newImage = anImg
+		var pixel = newImage.pixels
+		var factor: Float
+		
+		switch intensity {
+		case .Low:
+			factor = 0.6
+		case .Medium:
+			factor = 0.75
+		case .Strong:
+			factor = 1
+		}
+		
+		for index in 0..<newImage.pixels.count {
+			let pixelAvg = getPixelAvgVal(pixel[index], AlphaOn: false)
+			pixel[index].red = UInt8(Float(pixelAvg) * factor + Float(pixel[index].red) * (1 - factor))
+			pixel[index].green = UInt8(Float(pixelAvg) * factor + Float(pixel[index].green) * (1 - factor))
+			pixel[index].blue = UInt8(Float(pixelAvg) * factor + Float(pixel[index].blue) * (1 - factor))
+			pixel[index].alpha = UInt8(Float(pixelAvg) * factor + Float(pixel[index].alpha) * (1 - factor))
+			
 			newImage.pixels[index] = pixel[index]
 		}
 		return newImage
